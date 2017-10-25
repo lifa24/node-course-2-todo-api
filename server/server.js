22009e7d2967d9b7543c4e9a7951f40fac2e7c13
Faxiang Li <lifa0917@gmail.com>
2017-10-25 13:35:22 -0400

    Setup an app for heroku

--- server/server.js
+++ server/server.js
@@ -1,68 +1,56 @@
-var express=require('express');
-var bodyParser=require('body-parser');
-var {ObjectID}=require('mongodb');
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-var{mongoose}=require('./db/mongoose.js');
-var {Todo}=require('./models/todo');
-var {User}=require('./models/todo');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var app=express();
-const port=process.env.PORT|| 3000;
+var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
-    var todo=new Todo({
-      text:req.body.text
-    });
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-    todo.save().then((doc)=>{
-      res.send(doc);
-    },(e)=>{
-      res.status(400).send(e);
-    });
-});
-
-app.get('/todos',(req,res)=>{
-  Todo.find().then((todos)=>{
-    res.send({
-      todos
-    })
-  },(e)=>{
-    res.status(400).send(e);
-  })
-});
-
-app.get('./todos',(req,res)=>{
-  Todo.find().then((todos)=>{
-    res.send({todos});
-  },(e)=>{
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
     res.status(400).send(e);
   });
 });
 
-app.get('/todos/:id',(req,res)=>{
-  var id=req.params.id;
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
-  if(!ObjectID.isValid(id)){
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  Todo.findById(id).then((todo)=>{
-    if(!todo){
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
       return res.status(404).send();
     }
 
     res.send({todo});
-
-  }).catch((e)=>{
-      res.status(400).send();
-  })
-
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
-app.listen(port,()=>{
-  console.log(`Started up at port${port}`);
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
 
-module.exports={app};
+module.exports = {app};
