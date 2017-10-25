123b9470b7c1f73b05370c4088578e7c6affe124
Faxiang Li <lifa0917@gmail.com>
2017-10-25 13:26:55 -0400

    Setup app for heroku

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,32 +1,6 @@
-const {ObjectID}=require('mongodb');
+var mongoose =require('mongoose');
 
-const{mongoose}=require('./../server/db/mongoose');
-const{Todo}=require('./../server/models/todo');
-const {User}=require('./../server/models/user');
+mongoose.Promise=global.Promise;
+mongoose.connect (process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
 
-
-// var id='59f097b8c03ae54504d15227s';
-// if(!ObjectID.isValid(id)){
-//   console.log('ID not valid!');
-// }
-
-// Todo.find({
-//   _id:id
-// }).then((todos)=>{
-//   console.log('Todos',todos);
-// });
-// Todo.findById(id).then((todo)=>{
-//   if(!todo){
-//     return console.log('ID not found!');
-//   }
-//   console.log('Todo by id',todo);
-// }).catch((e)=>console.log(e));
-
-User.findById('59e15c449d584e53f41ec413').then((user)=>{
-  if(!user){
-    return console.log('Unable to find user!');
-  }
-  console.log(JSON.stringify(user,undefined,2))
-},(e)=>{
-console.log(e);
-});
+module.exports={mongoose};
